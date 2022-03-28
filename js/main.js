3ee71ff3935f32167b9101dcd84c08528d27c6c5
Doyeon <d_kim120803@fanshaweonline.ca>
2022-03-28 13:13:24 -0400

    building JS

--- js/main.js
+++ js/main.js
@@ -25,7 +25,57 @@
     let currentEl = event.dataTransfer.getData("draggedElement");
 		console.log(`dropped this element:`, currentEl);
     this.appendChild(document.querySelector(`#${currentEl}`));
+	
+	var file_nm = getSoundFile(currentEl);
+	var audio = new Audio("sounds/"+file_nm);
+
+	audio.play();
+
+	var dropZone1 = document.getElementById("dropZone1");
+	var dropZone2 = document.getElementById("dropZone2");
+	var dropZone3 = document.getElementById("dropZone3");
+	var dropZone4 = document.getElementById("dropZone4");
+	
+	var isFull = dropZone1.childNodes.length >= 1 && dropZone2.childNodes.length >= 1 && dropZone3.childNodes.length >= 1 && dropZone4.childNodes.length >= 1;
+	
+	if(isFull){
+		var iframe = document.getElementById('ani');
+		iframe.style.display = 'block';
+		document.getElementById('temp').style.display = 'none';
+	}else{
+		var iframe = document.getElementById('ani');
+		iframe.style.display = 'none';
+		document.getElementById('temp').style.display = 'block';
 	}
+  }
+
+  function getSoundFile(name) {
+	var file_nm = "";
+	
+	if(name == "Ajaeng"){
+		file_nm = "Ajaeng.mp3";
+	}else if(name == "Bass"){
+		file_nm = "bass_guitar.wav";
+	}else if(name == "Daeguem"){
+		file_nm = "Daeguem.avi.m4a";
+	}else if(name == "Gayageum"){
+		file_nm = "gayageum.mp3";
+	}else if(name == "Geomungo"){
+		file_nm = "Geomungo.m4a";
+	}else if(name == "Janggu"){
+		file_nm = "Janggu.mp3";
+	}else if(name == "Kkwaenggwari"){
+		file_nm = "Kkwaenggwari.mp3";
+	}else if(name == "Pansori"){
+		file_nm = "Pansori.mp4";
+	}else if(name == "Piano"){
+		file_nm = "Piano.m4a";
+	}else if(name == "Piri"){
+		file_nm = "Piri.m4a";
+	}
+
+	return file_nm;
+  }
 
 
   //add event handling
